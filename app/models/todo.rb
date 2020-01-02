31685fe06dca894c446f44fd33fc7057686cbc3f
Houston Knight <houstonknight3434@gmail.com>
2020-01-02 16:32:49 -0600

    Database migration

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,6 @@ class Todo < ApplicationRecord
   validates :name, presence: true
   # Validates that the Todo being created has a description
   validates :description, presence: true
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_193047) do
+ActiveRecord::Schema.define(version: 2019_12_11_231934) do
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
